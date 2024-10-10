16d3fedcae8712f7dcb096d0ebaadd4fc9b1e46f
hammit <85686274+hammit-01@users.noreply.github.com>
2024-10-11 00:19:17 +0900

    add files

---
+++ ShareDea/pages/signinpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../style/main.css"/>
+    <title>SHAREDEA</title>
+  </head>
+  <body>
+    <form method="post" action="../process/signin.php" onsubmit="return validateForm()">
+        <div class="login-wrapper">
+        <h1 onclick='main()'>ShareDea</h1>
+        <h6>Sign in</h6>
+        <input type='text' name='username' placeholder='Id' pattern="[A-Za-z0-9]{6,}" title="아이디는 영어와 숫자로 6글자 이상이어야 합니다." required>
+        <input type='password' name='password' id='password' placeholder='Password' pattern="(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*_])[A-Za-z\d!@#$%^&*_]{6,}" title="비밀번호는 영어와 숫자, 특수문자를 포함하여 6글자 이상이어야 합니다." required>
+        <input type='button' class='login_btn' value='Login' onclick='login()'>
+        <input type='submit' class='signin_btn' value='Signin'>
+        </div>
+        <script>
+            function main() {
+                window.location.href = 'main.php';
+            }
+            function login() {
+                window.location.href = 'mainpage.php';
+            }
+            function validateForm() {
+                const username = document.querySelector('input[name="username"]').value;
+                const password = document.querySelector('input[name="password"]').value;
+
+                const usernamePattern = /^[A-Za-z0-9]{6,}$/;
+                const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*_])[A-Za-z\d!@#$%^&*_]{6,}$/;
+
+                if (!usernamePattern.test(username)) {
+                    alert("아이디는 영어와 숫자로 6글자 이상이어야 합니다.");
+                    return false;
+                }
+
+                if (!passwordPattern.test(password)) {
+                    alert("비밀번호는 영어와 숫자, 특수문자를 포함하여 6글자 이상이어야 합니다.");
+                    return false;
+                }
+
+                return true;
+            }
+        </script>
+    </form>
+  </body>
+</html>
---
+++ ShareDea/process/liked.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+// 데이터베이스 연결 생성
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 데이터베이스 연결 확인
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+session_start(); // 세션 시작
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    $id = $_SESSION['contentid'];
+
+    $sql = "UPDATE text SET likes = likes + 1 WHERE id = '$id'";
+    if ($conn->query($sql) === TRUE) {
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+    echo "<script>
+        location.replace('../pages/contentpage.php');
+        </script>";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
